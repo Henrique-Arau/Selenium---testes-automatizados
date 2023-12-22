6dfc504c073d6745e8bf760b4d18deb7e6ffd6de
Henrique-Arau <penielh43@gmail.com>
2023-12-22 12:49:08 -0300

    Testando o cadastro de leilão

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -1,7 +1,10 @@
 package br.com.alura.leilao.leiloes;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.login.LoginPage;
@@ -11,10 +14,10 @@ public class LeiloesTest {
 	private LeiloesPage paginaDeLeiloes;
 	
 	
-	@AfterEach
-	public void afterEach() {
-		this.paginaDeLeiloes.fechar();
-	}
+//	@AfterEach
+//	public void afterEach() {
+//		this.paginaDeLeiloes.fechar();
+//	}
 	
 	
 	@Test
@@ -27,10 +30,14 @@ public class LeiloesTest {
 		
 		CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
 		
+		String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		String nome = "Leilao do dia " + hoje;
+		String valor = "500.00";
+		
+		this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+		
+		Assert.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
 		
 	}
-	
-	
-
 	
 }
