274fd0e59047fd1cd96252747fe9e3dd56fc2e0d
Henrique-Arau <penielh43@gmail.com>
2023-12-22 16:43:27 -0300

    Testando requisições AJAX (Codigo comentado)

--- src/test/java/br/com/alura/leilao/PageObject.java
+++ src/test/java/br/com/alura/leilao/PageObject.java
@@ -1,5 +1,7 @@
 package br.com.alura.leilao;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -16,6 +18,11 @@ public class PageObject {
     	} else {
     		this.browser = browser;	
     	}
+    	
+//    	Testes de requisição assincronas
+//    	this.browser.manage().timeouts()
+//    	.implicitlyWait(5, TimeUnit.SECONDS)
+//    	.pageLoadTimeout(10, TimeUnit.SECONDS);
 	}
 	
 	public void fechar() {
